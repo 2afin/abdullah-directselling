7baaf34d76ff5f4564aa4ab7b31a42ae8a75a0fa
2afin <koreanwerner@gmail.com>
2024-06-02 21:48:21 +0800

    Main.java

--- homework 1/Main.java
+++ homework 1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//----------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
 		
 		Client Faiq = new Client("Faiq", "123-456-7890", "Safin");  
 		
@@ -15,7 +15,7 @@ public class Main {
 	    ClientsList.printClient();
 	    ClientsList.findClient("Faiq");
 	    
-	    //----------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
 	    
 	    SaleTransaction transaction1 = new SaleTransaction("Product1", 2, 10.0, "2024-05-01");
 	    SaleTransaction transaction2 = new SaleTransaction("Product2", 1, 20.0, "2024-05-02");
@@ -35,7 +35,7 @@ public class Main {
         // Find a transaction by product name
         SaleTransaction foundTransaction = sales.find("Product1");
         
-        //----------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
 		
         SalesPerson sp1 = new SalesPerson("ronaldo", "011-22666999");
         
@@ -46,7 +46,7 @@ public class Main {
 		macai.printsp();
 		macai.findsp();
 		
-		//----------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
 		
 		System.out.println("abdul");
 	
