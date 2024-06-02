7fc519cb1cfe82a7f0e749e1ac7b971891591e3b
2afin <koreanwerner@gmail.com>
2024-06-02 21:50:38 +0800

    Add files via upload

---
+++ homework 1/SalesPersons.java
@@ -0,0 +1,29 @@
+package abdul;
+import java.util.*;
+public class SalesPersons {
+	
+    private ArrayList<SalesPerson> salespersons;
+    
+    public SalesPersons() {
+    	salespersons = new ArrayList<>(); 
+    }
+
+    public void addsp() {
+       
+    }
+
+    public void removesp() {
+        
+    }
+
+    public void printsp() {
+        
+    }
+
+    public void findsp() {
+        
+    }
+
+}
+
+//DANISH SAFIN BIN ZULKARNAIN 24000149
